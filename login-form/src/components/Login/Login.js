93174eeb536199915e394f39e9a017cbe86942b4
minajchy <minajchy.123@gmail.com>
2024-02-24 10:48:31 +0530

    login-finished

--- login-form/src/components/Login/Login.js
+++ login-form/src/components/Login/Login.js
@@ -1,8 +1,10 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer, useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from "../../store/auth-context";
+import Input from "../UI/Input/Input";
 
 const emailReducer = (state, action) => {
   if (action.type === "USER_INPUT") {
@@ -43,7 +45,9 @@ const Login = (props) => {
     isValid: null,
   });
 
-  useEffect(() => { 
+  const authCtx = useContext(AuthContext);
+
+  useEffect(() => {
     console.log("Effect Running");
 
     return () => {
@@ -57,9 +61,7 @@ const Login = (props) => {
   useEffect(() => {
     const identifier = setTimeout(() => {
       console.log("checking form validity");
-      setFormIsValid(
-        emailIsValid && passwordIsValid
-      );
+      setFormIsValid(emailIsValid && passwordIsValid);
     }, 500);
 
     return () => {
@@ -85,10 +87,6 @@ const Login = (props) => {
     // setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
   };
 
-  /*const collegenameChangeHandler = (event) => {
-    setEnteredCollegename(event.target.value);
-  };*/
-
   const validateEmailHandler = () => {
     dispatchEmail({ type: "INPUT_BLUR" });
     //setEmailIsValid(emailState.isValid);
@@ -99,60 +97,35 @@ const Login = (props) => {
     //setPasswordIsValid(enteredPassword.trim().length > 6);
   };
 
-  /*const validateCollageName = () => {
-    setCollegenameIsValid(enteredCollegename.trim().length > 0);
-  };*/
-
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value /*enteredCollegename*/);
+    authCtx.onLogin(
+      emailState.value,
+      passwordState.value 
+    );
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${
-            emailState.isValid === false ? classes.invalid : ""
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            value={emailState.value}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
-          />
-        </div>
-        <div
-          className={`${classes.control} ${
-            passwordState.isValid === false ? classes.invalid : ""
-          }`}
-        >
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={passwordState.value}
-            onChange={passwordChangeHandler}
-            onBlur={validatePasswordHandler}
-          />
-        </div>
-        {/* <div
-          className={`${classes.control} ${
-            collegenameIsValid === false ? classes.invalid : ""
-          }`}
-        >
-          <label htmlFor="collegename">College Name</label>
-          <input
-            type="text"
-            id="collegename"
-            value={enteredCollegename}
-            onChange={collegenameChangeHandler}
-            onBlur={validateCollageName}
-          />
-        </div> */}
+        <Input
+          id="email"
+          label="E-Mail"
+          type="email"
+          isValid={emailIsValid}
+          value={emailState.value}
+          onChange={emailChangeHandler}
+          onBlur={validateEmailHandler}
+        ></Input>
+        <Input
+          id="password"
+          label="password"
+          type="password"
+          isValid={passwordIsValid}
+          value={passwordState.value}
+          onChange={passwordChangeHandler}
+          onBlur={validatePasswordHandler}
+        ></Input>
         <div className={classes.actions}>
           <Button type="submit" className={classes.btn} disabled={!formIsValid}>
             Login
